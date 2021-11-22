2e0889aa5495f5df77b7f0b46ec362218b52320e
Harini-Sritharar <harinisri2004@gmail.com>
2021-11-22 21:40:43 +0000

    Added the multiple choice answers to the question screen

--- Iteration 1/lib/screens/quiz_questions_screen.dart
+++ Iteration 1/lib/screens/quiz_questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:nea_prototype_1/checkbox_tile.dart';
 //import '../button.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -7,42 +8,46 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  bool _checkboxListTile = false;
+  //bool _checkboxListTile = false;
+  int numOfOptions = 4;
+  late List<bool> _checkBoxSelected;
+  late List<String> _checkBoxMessages;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkBoxSelected = List.filled(numOfOptions, false);
+    _checkBoxMessages = ["cos x","sin x", "tan x", "-sin x"];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text( "What is dy/dx of cos x?"),
-        centerTitle: true,
-      ),
-      body:(
-        Column(
-          children: [
-        CheckboxListTile(
-          controlAffinity: ListTileControlAffinity.leading,
-          title: Text("sin x"),
-          value: _checkboxListTile,
-          onChanged: (value){
-            setState(() {
-            _checkboxListTile =! _checkboxListTile;
-            });
-          }
-       ),
-        CheckboxListTile(
-        controlAffinity: ListTileControlAffinity.leading,
-        title: Text("cos x"),
-        value: _checkboxListTile,
-        onChanged: (value){
-          setState(() {
-            _checkboxListTile =! _checkboxListTile;
-          });
-        },
-      )
-          ]
-
-
-       )
-      )
-    );
+        appBar: AppBar(
+          title: Text("What is dy/dx of cos x?"),
+          centerTitle: true,
+        ),
+        body: (Column(children: [
+          for (var i = 0; i < numOfOptions; i++)
+            CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title: Text(_checkBoxMessages[i]),
+                value: _checkBoxSelected[i],
+                onChanged: (value) {
+                  setState(() {
+                    _checkBoxSelected[i] =! _checkBoxSelected[i];
+                  });
+                }),
+          // CheckboxListTile(
+          // controlAffinity: ListTileControlAffinity.leading,
+          // title: Text("cos x"),
+          // value: _checkboxListTile,
+          // onChanged: (value){
+          //   setState(() {
+          //     _checkboxListTile =! _checkboxListTile;
+          //   });
+          // },
+          // )
+        ])));
   }
 }
