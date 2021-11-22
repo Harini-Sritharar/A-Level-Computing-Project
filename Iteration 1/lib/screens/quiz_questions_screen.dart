42aa5719d6f2f2fe5035a5fac506266658c628a5
Harini-Sritharar <harinisri2004@gmail.com>
2021-11-22 12:26:26 +0000

    Changed the Column to Scaffold + added checkboxes for multiple choice answers

--- Iteration 1/lib/screens/quiz_questions_screen.dart
+++ Iteration 1/lib/screens/quiz_questions_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../button.dart';
+//import '../button.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -8,70 +7,25 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  
-  var _indexQuestion = 0;
-
-  var _questions = ['What is dy/dx of sin x?', 'What is dy/dx of cos x?'];
-  var _answers = [
-    ['cos x', '-cos x'],
-    ['sin x', '-sin x'],
-  ];
-
-  _nextq() {
-    setState(() {
-      var lastIndex = _questions.length - 1;
-      if (_indexQuestion < lastIndex) {
-        _indexQuestion++;
-      } else {
-        // Is the last question
-      }
-    });
-   }
-
-_backq() {
-    setState(() {
-      //var lastIndex = _questions.length - 1;
-      if (_indexQuestion >= 1) {
-        _indexQuestion--;
-      } else {
-        // Is the last question
-      }
-    });
-   }
+  bool checkboxListTile = false;
   @override
   Widget build(BuildContext context) {
-     AppBar(
-      automaticallyImplyLeading: true,
-    );
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(        
-        child: Text(_questions[_indexQuestion])),
-        for (var answer in _answers[_indexQuestion]) Text(answer),
-        //function next
-          QuizButton("Back", _backq),
-          QuizButton("Next", _nextq)
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( "What is dy/dx of cos x?"),
+      ),
+      body:(
+      CheckboxListTile(
+        controlAffinity: ListTileControlAffinity.leading,
+        title: Text("sin x"),
+        value: checkboxListTile,
+        onChanged: (value){
+          setState(() {
+            checkboxListTile = !checkboxListTile;
+          });
+        },
+      )
+       )
     );
   }
 }
-  // @override
-  // Widget build(BuildContext context) {    
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       actions: []),
-  //     );
-      
-  //     }
-
-
-    // return Stack(
-    //   children: [
-    //     SafeArea(
-    //       child: Padding(
-    //         padding: const EdgeInsets.symmetric(horizontal: 5,) ,)
-    //         )
-    //         ]
-    //     )
-    // ;
