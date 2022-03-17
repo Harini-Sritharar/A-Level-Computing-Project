a9b9072950670756eb9f74fd374656ace932df25
Harini-Sritharar <harinisri2004@gmail.com>
2022-03-17 22:21:07 +0000

    Improved UI

--- Iteration 1/lib/screens/Quiz/Menus/by_you_quizzes_menu.dart
+++ Iteration 1/lib/screens/Quiz/Menus/by_you_quizzes_menu.dart
@@ -1,11 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:nea_prototype_1/button.dart';
 import 'package:nea_prototype_1/main.dart';
-import 'package:nea_prototype_1/models/questionInfo.dart';
 import 'package:nea_prototype_1/models/quiz.dart';
 import 'package:nea_prototype_1/screens/Quiz/Navigation/quiz_navigator.dart';
-import 'package:nea_prototype_1/services/database.dart';
+import 'package:random_string/random_string.dart';
 
 class ByYouMenu extends StatefulWidget {
   ByYouMenu({
@@ -14,38 +11,6 @@ class ByYouMenu extends StatefulWidget {
   @override
   _ByYouMenuState createState() => _ByYouMenuState();
 }
-
-// addToClass(BuildContext context) {
-//   final _formKey = GlobalKey<FormState>();
-//   return showDialog(
-//       context: context,
-//       builder: (context) {
-//         var subjectController;
-//         return AlertDialog(
-//           title: Text('Add Quiz'),
-//           content: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               Form(
-//                   key: _formKey,
-//                   child: Column(
-//                     children: [
-//                       TextFormField(
-//                         controller: subjectController,
-//                         validator: (val) {
-//                           return val!.isEmpty ? "Subject" : null;
-//                         },
-//                         decoration: InputDecoration(hintText: "Subject"),
-//                       ),
-//                     ],
-//                   ))
-//             ],
-//           ),
-//           actions: [],
-//         );
-//       });
-// }
-
 class _ByYouMenuState extends State<ByYouMenu> {
   Widget createCard(i) {
     return GestureDetector(
@@ -55,8 +20,11 @@ class _ByYouMenuState extends State<ByYouMenu> {
         //width: 150,
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
-            color: Colors.purple[100], borderRadius: BorderRadius.circular(16) ),
-          margin: EdgeInsets.symmetric(horizontal: 5),
+            //color: Colors.purple[100],
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [Colors.purple,Colors.teal]) ),
+          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
 
         child: Center(
           child: Column(
@@ -76,32 +44,6 @@ class _ByYouMenuState extends State<ByYouMenu> {
       },
     );
   }
-  // Widget buildCard(int i) {
-  //   int value = i;
-  //   return GestureDetector(
-  //     child: Container(
-  //       color: Colors.red,
-  //       margin: EdgeInsets.symmetric(horizontal: 5),
-  //       width: 200,
-  //       height: 200,
-  //       child: Center(
-  //           child: Column(
-  //         children: [
-  //           Spacer(),
-  //           Text(appUser.classes[i].className),
-  //           Text(appUser.classes[i].subject),
-  //           Spacer(),
-  //         ],
-  //       )),
-  //     ),
-  //     onTap:(){
-  //     Navigator.push(
-  //                   context,
-  //                   MaterialPageRoute(
-  //                     builder: (context) =>  ClassInfoScreen(value)));}
-  //   );
-  // }
-
   List<Quiz> userQuizzes = appUser.quizzes;
   @override
   Widget build(BuildContext context) {
@@ -109,115 +51,28 @@ class _ByYouMenuState extends State<ByYouMenu> {
         appBar: AppBar(
           title: Text("By You"),
           centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple,Colors.blue],
+              )
         ),
+          )),
         body: Container(
-          height: 200,
-
-          child: ListView(
-            scrollDirection: Axis.horizontal,
+          height: 400,
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              ),
             children:  [
                   //Spacer(),
                   for (int i = 0; i < appUser.quizzes.length; i++) createCard(i),
                   //Spacer()
             ],
           ),
-        )
-
-
-
-
-
-
-        
-        // ListView(
-        //         scrollDirection: Axis.horizontal,
-        //         children: [
-        //           Container(
-        //             height: 500,
-        //             child: GridView(
-        //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //             crossAxisCount: 2,
-        //             crossAxisSpacing: 10,
-        //             mainAxisSpacing: 10),
-        //         children: [
-        //           for (int i = 0; i < userQuizzes.length; i++)
-        //           createCard(i),
-        //           Text("i"),
-
-        //           //QuizCard.createCard(i: i)
-        //         ],
-        //       ),
-        //     )
-        //          // for (int i = 0; i < appUser.classes.length; i++) createCard(i)
-        //         ],
-        //       ),
-        // // body: ListView(
-        //   physics: BouncingScrollPhysics(),
-        //   padding: const EdgeInsets.all(15),
-        //   children: [
-        //     SizedBox(height: 10),
-        //     Container(
-        //       height: 500,
-        //       child: GridView(
-        //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //             crossAxisCount: 2,
-        //             crossAxisSpacing: 10,
-        //             mainAxisSpacing: 10),
-        //         children: [
-        //           for (int i = 0; i < userQuizzes.length; i++) createCard(i),
-        //           //QuizCard.createCard(i: i)
-        //         ],
-        //       ),
-        //     )
-        //   ],
-        // )
-
-        // body: Center(
-        //   child: Column(
-        //     children: [
-        //       SizedBox(height: 35),
-
-        //       for (int i = 0; i < (userQuizzes.length); i++)
-        //         TextButton(
-        //             onPressed: () {
-        //               Navigator.push(
-        //                   context,
-        //                   MaterialPageRoute(
-        //                       builder: (context) =>
-        //                           QuizNavigator(userQuizzes[i].questions)));
-        //             },
-        //             //child:
-        //             child: new Card(
-        //                 child: Column(
-        //               mainAxisSize: MainAxisSize.min,
-        //               children: <Widget>[
-        //                 ListTile(
-        //                   leading: Icon(Icons.question_answer),
-        //                   title: Text(userQuizzes[i].quizTitle),
-        //                   //onLongPress: addToClass(context),
-        //                   onLongPress: () {
-        //                     Navigator.push(
-        //                         context,
-        //                         MaterialPageRoute(
-        //                             builder: (context) =>
-        //                                 QuizNavigator(userQuizzes[i].questions)));
-        //                   },
-        //                 ),
-        //                 // Row(
-        //                 //   children: [
-        //                 //     Icon(Icons.question_answer),
-        //                 //     Text(userQuizzes[i].quizTitle,
-        //                 //     ),
-        //                 //     SizedBox(width: 50,),
-        //                 // GestureDetector(
-        //                 //   child: Icon(Icons.add)
-        //                 // )],)
-        //               ],
-        //             )))
-        //     ],
-        //   ),
-        // ),
+      
+            
+          ),
         );
-    // return
   }
 }
--- Iteration 1/lib/screens/profile_screen.dart
+++ Iteration 1/lib/screens/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nea_prototype_1/button.dart';
 import '../main.dart';
 import 'Authentication/welcome_screen.dart';
 
@@ -9,82 +8,116 @@ class ProfileScreen extends StatefulWidget {
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
- 
+
 class _ProfileScreenState extends State<ProfileScreen> {
+  TextStyle nameStyle =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white);
+  TextStyle fieldStyle =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black);
+
+  Widget welcomeGreeting() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text("Hi", style: TextStyle(fontSize: 14, color: Colors.white)),
+      Text("${appUser.name}", style: nameStyle),
+    ]);
+  }
+
+  Widget buildProfilePicture() {
+    return Container(
+      child: Center(
+          child: ClipOval(
+        child: Material(
+          color: Colors.purple,
+          child: Ink.image(
+            alignment: Alignment.centerLeft,
+            image:
+                NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
+            fit: BoxFit.cover,
+            width: 128,
+            height: 128,
+          ),
+        ),
+      )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    //final String initial = appUser.name[0];
-    TextStyle myStyle = TextStyle(color: Colors.white);
-    //name = myController.text;
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(appUser.name + "'s Profile"),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 45.0),
-            CircleAvatar(
-              backgroundImage: NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
-              //backgroundImage: NetworkImage("https:pngtree.com/freepng/flat-user-avatar-astronaut_4720959.html"),
-              radius: 50,
-            ),
-            SizedBox(height:20),         
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color:Colors.teal[20],
-                borderRadius: BorderRadius.circular(40),
-                boxShadow: [
-                  BoxShadow(
-                  color: Color(0xFF42A5F5), spreadRadius: 3
-                  )]),    
-              child: Column(
-                children: [
-            Text(
-              "Name: " + appUser.name,
-              textScaleFactor: 1.5,
-              style: myStyle,
-            ),
-            SizedBox(height:20),         
-            Text(
-              "Email: " + appUser.email,
-              textScaleFactor: 1.5,
-              style: myStyle,
-            ),
-           
-            SizedBox(height:20),         
-            Text(
-              "Position: " + appUser.position,
-              textScaleFactor: 1.5,
-              style: myStyle,
-            ),
-            SizedBox(height:20),
-            
-            Text((appUser.position == "student")
-                      ? ("Year Group: ${appUser.yearGroup}") : '',
-             textScaleFactor: 1.5,
-              style: myStyle),
-            ]
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Colors.purple, Colors.pink],
             )),
-            SizedBox(height:200),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-            onPressed: ()  async {await authService.signOut();
-                Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
-                    MaterialPageRoute(
-                      builder: (context) => WelcomeScreen(),
-                    ),
-                    (route) => false);
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Container(
+                decoration: BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Colors.purple, Colors.pink]),
+                ),
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.centerLeft,
+                child: welcomeGreeting()),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              await authService.signOut();
+              Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => WelcomeScreen(),
+                  ),
+                  (route) => false);
             },
-            child: const Icon(Icons.logout))
-            )
-          ])
-      ),
-    );
+            child: const Icon(Icons.logout)),
+        body: ListView(physics: BouncingScrollPhysics(), children: [
+          SizedBox(
+            height: 30,
+          ),
+          buildProfilePicture(),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Container(
+                width: 50,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    boxShadow: [
+                      BoxShadow(color: Color(0xFF42A5F5), spreadRadius: 3)
+                    ]),
+                child: Column(children: [
+                  Text(
+                    "Name: " + appUser.name,
+                    textScaleFactor: 1.5,
+                    style: fieldStyle,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Email: " + appUser.email,
+                    textScaleFactor: 1.5,
+                    style: fieldStyle,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Position: " + appUser.position,
+                    textScaleFactor: 1.5,
+                    style: fieldStyle,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                      (appUser.position == "student")
+                          ? ("Year Group: ${appUser.yearGroup}")
+                          : '',
+                      textScaleFactor: 1.5,
+                      style: fieldStyle),
+                ])),
+          )
+        ]));
   }
 }
