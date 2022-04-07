2f04bdad833430037d98a1bade148cc683a980d9
Harini-Sritharar <harinisri2004@gmail.com>
2022-04-07 09:23:23 +0100

    Commented Home Screen code

--- Iteration 1/lib/screens/home_screen.dart
+++ Iteration 1/lib/screens/home_screen.dart
@@ -1,11 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nea_prototype_1/screens/Quiz/Menus/by_you_quizzes_menu.dart';
-import 'package:nea_prototype_1/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:nea_prototype_1/button.dart';
-import 'package:nea_prototype_1/screens/Authentication/welcome_screen.dart';
-
 import 'package:nea_prototype_1/screens/profile_screen.dart';
 import '../main.dart';
 import 'Quiz/Menus/preset_quizzes_menu.dart';
@@ -25,47 +21,45 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
-  final Stream<QuerySnapshot> users =
-      Firestore.instance.collection("Users").snapshots();
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
+          // shows the user's name in the app bar
           title: Text(appUser.name + "'s Home Page"),
-          centerTitle: true,
+          centerTitle: true, // centres the title
         ),
         body: Center(
             child: Container(
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment
+                        .center, // all widgets are aligned in the ceetre of the page
                     children: <Widget>[
               GenericButton("Profile", () {
+                // Profile Button
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => ProfileScreen()));
               }),
               SizedBox(height: 35.0),
               GenericButton("Leaderboard", () {
+                // Leaderboard Button
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                   content: Text('Awesome feature is in the works! 😎'),
                 ));
               }),
               SizedBox(height: 35.0),
               GenericButton("Pre Set", () {
+                // Preset Button
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => PresetMenu()));
               }),
               SizedBox(height: 35.0),
               GenericButton("By You", () {
+                // By You Button
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => ByYouMenu()));
               }),
               SizedBox(height: 75.0),
-
             ]))));
   }
 }
-
-  // ],
-  // ))
-
-
