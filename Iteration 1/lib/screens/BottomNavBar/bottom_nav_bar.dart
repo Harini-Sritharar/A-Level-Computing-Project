22f5fd7e47f5b8aa08ca03f9b3f62cb3e1c5e43f
Harini-Sritharar <harinisri2004@gmail.com>
2022-04-07 19:29:13 +0100

    Commented Bottom Nav Bar Code

--- Iteration 1/lib/screens/BottomNavBar/bottom_nav_bar.dart
+++ Iteration 1/lib/screens/BottomNavBar/bottom_nav_bar.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:nea_prototype_1/screens/Authentication/welcome_screen.dart';
 import 'package:nea_prototype_1/screens/Quiz/Create%20Quiz/create_quiz.dart';
 import 'package:nea_prototype_1/screens/Quiz/Menus/by_you_quizzes_menu.dart';
 import 'package:nea_prototype_1/screens/Quiz/Menus/preset_quizzes_menu.dart';
 import 'package:nea_prototype_1/screens/Classes/classes_screen.dart';
-import 'package:nea_prototype_1/screens/home_screen.dart';
 import 'package:nea_prototype_1/screens/profile_screen.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
-
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
@@ -15,10 +12,10 @@ class BottomNavBar extends StatefulWidget {
   @override
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
-
 class _BottomNavBarState extends State<BottomNavBar> {
-  final PersistentTabController controller =
-      PersistentTabController(initialIndex: 0);
+  final PersistentTabController controller = PersistentTabController(
+      initialIndex: 0); //sets the initial index of the controller to 0
+  //function which returns a list of all the screens in the navigation bar
   List<Widget> buildScreens() {
     return [
       ProfileScreen(),
@@ -28,16 +25,16 @@ class _BottomNavBarState extends State<BottomNavBar> {
       ByYouMenu(),
     ];
   }
-
+  // function which returns a list of all the items and icons in the navigation bar
   List<PersistentBottomNavBarItem> buildItems() {
     return [
       PersistentBottomNavBarItem(
           icon: Icon(Icons.account_circle_rounded), title: "Profile"),
-      PersistentBottomNavBarItem(icon: Icon(Icons.class__outlined),title: "Classes"),
       PersistentBottomNavBarItem(
-          icon: Icon(Icons.add,color: Colors.white), title: "Create"),
+          icon: Icon(Icons.class__outlined), title: "Classes"),
       PersistentBottomNavBarItem(
-          icon: Icon(Icons.quiz), title: "Preset"),
+          icon: Icon(Icons.add, color: Colors.white), title: "Create"),
+      PersistentBottomNavBarItem(icon: Icon(Icons.quiz), title: "Preset"),
       PersistentBottomNavBarItem(
           icon: Icon(Icons.question_answer_outlined), title: "By You"),
     ];
@@ -52,31 +49,19 @@ class _BottomNavBarState extends State<BottomNavBar> {
       items: buildItems(),
       confineInSafeArea: true,
       backgroundColor: Colors.white,
-      handleAndroidBackButtonPress: true,
+      handleAndroidBackButtonPress: true, // handles the actions of the back button on Android
       resizeToAvoidBottomInset:
-          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-      stateManagement: true, // Default is true.
+          true, // Moves the screen up when keyboard appears. 
+      stateManagement: true, // Preserves the state of each tab screen
       hideNavigationBarWhenKeyboardShows:
-          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-      decoration: NavBarDecoration(
+          true, // hides the navigation bar when the onscreen keyboard is open
+      decoration: NavBarDecoration( // decoration for the naviagation bar
         borderRadius: BorderRadius.circular(10.0),
-        colorBehindNavBar: Colors.white,
+        colorBehindNavBar: Colors.white,// colour for the container which holds the navigation bar
       ),
       popAllScreensOnTapOfSelectedTab: true,
       popActionScreens: PopActionScreensType.all,
-      // itemAnimationProperties: ItemAnimationProperties(
-      //   // Navigation Bar's items animation properties.
-      //   duration: Duration(milliseconds: 200),
-      //   curve: Curves.ease,
-      // ),
-      // // screenTransitionAnimation: ScreenTransitionAnimation(
-      // //   // Screen transition animation on change of selected tab.
-      // //   animateTabTransition: true,
-      // //   curve: Curves.ease,
-      // //   duration: Duration(milliseconds: 200),
-      // // ),
-
-      navBarStyle: NavBarStyle.style15,
+      navBarStyle: NavBarStyle.style15, // this style allows 5 icons in the navigation bar 
     );
   }
 }
