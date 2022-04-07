15a923be9c19d2a6f1cd2f8c5a89ab809cb4bc76
Harini-Sritharar <harinisri2004@gmail.com>
2022-04-07 09:13:20 +0100

    Commented Quiz Navigation code

--- Iteration 1/lib/screens/Quiz/Navigation/quiz_navigator.dart
+++ Iteration 1/lib/screens/Quiz/Navigation/quiz_navigator.dart
@@ -5,14 +5,16 @@ import 'package:nea_prototype_1/screens/Quiz/Navigation/quiz_screen_function.dar
 import 'package:random_string/random_string.dart';
 
 class QuizNavigator extends StatefulWidget {
+  // parameter for the class
   final List<QuestionInfo> questionsAndOptions;
-
+  // constructor for the class
   QuizNavigator(this.questionsAndOptions);
   @override
   _QuizNavigatorState createState() => _QuizNavigatorState();
 }
 
 class _QuizNavigatorState extends State<QuizNavigator> {
+  // initialising variables to default values
   int currentQ = 0;
   int points = 0;
   bool endOfQuiz = false;
@@ -23,16 +25,17 @@ class _QuizNavigatorState extends State<QuizNavigator> {
   @override
   void initState() {
     super.initState();
+    // for each question in the questionsAndOptions, create a QuizScreen and give it these paramaters shown below
     for (int i = 0; i < widget.questionsAndOptions.length; i++) {
       questions.add(QuizScreen(widget.questionsAndOptions[i], setNextQuestion,
           returnPreviousQuestion, checkQuizEnd,randomBetween(1, 1000000),checkLenQuiz));
     }
   }
-
+  // function which adds the value of newPoints to the points variable
   void addPoints(int newPoints) {
     points += newPoints;
   }
-
+  // function which naviagtes the user to the next question when they click 'skip' or 'submit'
   void setNextQuestion() {
     setState(() {
       if (currentQ < questions.length - 1) {
@@ -42,7 +45,7 @@ class _QuizNavigatorState extends State<QuizNavigator> {
       }
     });
   }
-
+  // function which naviagtes the user to the previous question when they click 'back'
   void returnPreviousQuestion() {
     setState(() {
       if (currentQ != 0) {
@@ -50,14 +53,15 @@ class _QuizNavigatorState extends State<QuizNavigator> {
       }
     });
   }
-  checkLenQuiz(){
+  // function which returns the length of the quiz
+  int checkLenQuiz(){
     return questions.length;
   }
-
-  checkQuizEnd() {
+  // function which checks if the quiz has ended
+  bool checkQuizEnd() {
     return (currentQ + 1 == questions.length);
   }
-
+  // build function which returns all the questions of the current quiz
   Widget build(BuildContext context) {
     return questions[currentQ];
   }
--- Iteration 1/lib/screens/Quiz/Navigation/quiz_screen_function.dart
+++ Iteration 1/lib/screens/Quiz/Navigation/quiz_screen_function.dart
@@ -1,5 +1,5 @@
+// import dependencies
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:nea_prototype_1/button.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -7,22 +7,21 @@ import 'package:nea_prototype_1/main.dart';
 import 'package:nea_prototype_1/models/questionInfo.dart';
 import 'package:nea_prototype_1/screens/Quiz/score_screen.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
 class QuizScreen extends StatefulWidget {
-  //int points = 0;
+  // parameters
   final void Function() setNextQuestion;
   final void Function() returnPreviousQuestion;
   final Function() checkQuizEnd;
-  //final Function(int newPoints) addPoints;
   final Function() checkLenQuiz;
   final int seed;
-
   final QuestionInfo questionInfo;
+  // constructor
   QuizScreen(
       this.questionInfo,
       this.setNextQuestion,
       this.returnPreviousQuestion,
       this.checkQuizEnd,
-     // this.addPoints,
       this.seed,
       this.checkLenQuiz);
 
@@ -32,49 +31,59 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
   late final int numOfIncorrectOptions;
-  int points = 0;
   late final List<bool> _checkBoxSelected;
+  int points = 0;
   int numOfQs = 0;
-
+  int chosenAnswer = 0;
 //
   @protected
   @mustCallSuper
+  // this function sets the 3 variables to the values depending on the current question
   void initState() {
     numOfIncorrectOptions = widget.questionInfo.incorrectOptions.length;
     _checkBoxSelected = List.filled(numOfIncorrectOptions + 1, false);
     numOfQs = widget.checkLenQuiz();
   }
 
-  int chosenAnswer = 0;
   checkAnswer(int chosenAns) {
+    // the default message and bgColour are declared here
     String message = "Correct";
     Color? bgColour = Colors.green[300];
+    // if the answer chosen is correct, the points increment
     if (chosenAns == 0) {
       points += 10;
     } else {
+      // if the answer chosen is incorrect, the default message and bgColour change
       bgColour = Colors.red[700];
       message = "Incorrect";
     }
     return showDialog(
+        // returns a Dialog
         barrierDismissible: true,
         context: context,
         builder: (BuildContext context) {
+          // if the quiz had not ended yet
           if (!widget.checkQuizEnd()) {
             return GestureDetector(
+              // when the dialog is tapped, the next question is loaded in
               onTap: () {
                 widget.setNextQuestion();
                 Navigator.pop(context);
               },
+              // creates an AlertDialog with the relevant background colour and message
               child: AlertDialog(
                 backgroundColor: bgColour,
                 title: Text(message),
               ),
             );
-          } else {
+          }
+          // if current question is the last question
+          else {
             return GestureDetector(
               child:
                   AlertDialog(backgroundColor: bgColour, title: Text(message)),
               onTap: () {
+                // navigate the user to the ScoreScreen
                 pushNewScreen(context,
                     screen: ScoreScreen(points, numOfQs), withNavBar: true);
               },
@@ -83,8 +92,11 @@ class _QuizScreenState extends State<QuizScreen> {
         });
   }
 
+  // function which build the options layout of the screen
   List<Widget> buildOptions() {
+    // initialises an empty list called options
     List<RadioListTile> options = [];
+    // adds the RadioListTile representing the correct answer
     options.add(RadioListTile(
       title: Text(widget.questionInfo.correctOption),
       value: 0,
@@ -99,6 +111,7 @@ class _QuizScreenState extends State<QuizScreen> {
         });
       },
     ));
+    // adds the RadioListTiles representing the incorrect answers
     for (int i = 0; i < numOfIncorrectOptions; i++) {
       options.add(RadioListTile(
         title: Text(widget.questionInfo.incorrectOptions[i]),
@@ -115,13 +128,7 @@ class _QuizScreenState extends State<QuizScreen> {
         },
       ));
     }
-    //Random(seed) creates a random generator which is an object used
-    // by any random object to create a random sequence. Based on the seed,
-    // a sequence of random numbers will be created, because I want the ssame sequence
-    // to be generated each time, i shuffle it with Random(seed) where the seed is generated
-    // once and not changed. It is needed because when then user clicks on a option
-    // the screen is reloaded with the change in effect but this also reshuffles the options
-    // i wnat to reshuffle the options with the same seed so that the order doesn't change
+    // shuffles the options using Random(seed)
     options.shuffle(Random(widget.seed));
     return options;
   }
@@ -132,51 +139,11 @@ class _QuizScreenState extends State<QuizScreen> {
       appBar: AppBar(
         title: AutoSizeText(widget.questionInfo.questionName, maxLines: 1),
         centerTitle: true,
+        // removes the back button from the app bar
         automaticallyImplyLeading: false,
       ),
       body: (Stack(children: [
-        // // countdown timer => not essential at the moment and so can be included once essential features are completed
-        // SafeArea(
-        //   child: Padding(
-        //     padding: const EdgeInsets.symmetric(horizontal: 50, vertical:50 ),
-        //     child: Column(
-        //       children: [
-        //         Container(
-        //           width: double.infinity,
-        //           height: 50,
-        //           decoration: BoxDecoration(
-        //             border: Border.all(color: Colors.blue,
-        //             width: 4),
-        //             borderRadius: BorderRadius.circular(50)
-        //           ),
-        //           child: Stack(children: [
-        //             LayoutBuilder(
-        //               builder: (context,constraints) => Container(
-        //                 width: constraints.maxWidth * 0.5,
-        //                 decoration: BoxDecoration(
-        //                   gradient: LinearGradient(
-        //                     colors: [Color(0xFFB2DFDB), Color(0xFF00796B)],
-        //                     begin: Alignment.centerLeft,
-        //                     end: Alignment.centerRight),
-        //                   borderRadius: BorderRadius.circular(50)
-        //                 ),
-        //               )),
-        //           Positioned.fill(
-        //             child: Padding(
-        //               padding: const EdgeInsets.symmetric(horizontal: 25),
-        //               child: Row(
-        //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //                 children: [
-        //                   Text(" 20 seconds")
-        //                 ],)
-        //             )
-        //               )
-        //           ],)
-        //         )
-        //       ],
-        //     ),)),
         // creating the radio buttons
-        //Divider(height: 30, indent: 10),
         SizedBox(height: 70),
         Container(
           padding: EdgeInsets.all(10),
@@ -187,7 +154,9 @@ class _QuizScreenState extends State<QuizScreen> {
               ),
               for (Widget answer in buildOptions())
                 customWidgets.customContainer(answer),
+              // Takes the user to the next question
               QuizButton("Skip", widget.setNextQuestion),
+              // runs the checkAnswer function on the answer chosen by the user
               QuizButton("Submit", () {
                 checkAnswer(chosenAnswer);
               })
@@ -198,3 +167,44 @@ class _QuizScreenState extends State<QuizScreen> {
     );
   }
 }
+
+// // countdown timer => not essential at the moment and so can be included once essential features are completed
+// SafeArea(
+//   child: Padding(
+//     padding: const EdgeInsets.symmetric(horizontal: 50, vertical:50 ),
+//     child: Column(
+//       children: [
+//         Container(
+//           width: double.infinity,
+//           height: 50,
+//           decoration: BoxDecoration(
+//             border: Border.all(color: Colors.blue,
+//             width: 4),
+//             borderRadius: BorderRadius.circular(50)
+//           ),
+//           child: Stack(children: [
+//             LayoutBuilder(
+//               builder: (context,constraints) => Container(
+//                 width: constraints.maxWidth * 0.5,
+//                 decoration: BoxDecoration(
+//                   gradient: LinearGradient(
+//                     colors: [Color(0xFFB2DFDB), Color(0xFF00796B)],
+//                     begin: Alignment.centerLeft,
+//                     end: Alignment.centerRight),
+//                   borderRadius: BorderRadius.circular(50)
+//                 ),
+//               )),
+//           Positioned.fill(
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 25),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text(" 20 seconds")
+//                 ],)
+//             )
+//               )
+//           ],)
+//         )
+//       ],
+//     ),)),
