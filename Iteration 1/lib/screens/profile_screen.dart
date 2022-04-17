3dedc3a8f5df38669f29bd5ac97e2c0b0591d441
Harini-Sritharar <harinisri2004@gmail.com>
2022-04-17 17:09:15 +0100

    Commented the Profile Screen

--- Iteration 1/lib/screens/profile_screen.dart
+++ Iteration 1/lib/screens/profile_screen.dart
@@ -1,48 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:nea_prototype_1/button.dart';
-import 'package:nea_prototype_1/custom_widgets.dart';
-import 'package:nea_prototype_1/screens/BottomNavBar/bottom_nav_bar.dart';
 import 'package:nea_prototype_1/screens/Walkthrough/walkthrough.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import '../main.dart';
 import 'Authentication/welcome_screen.dart';
-
-//import 'package:nea_prototype_1/screens/signUp_screen.dart';
 class ProfileScreen extends StatefulWidget {
   ProfileScreen({Key? key}) : super(key: key);
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
-
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floating action button for sign out
         floatingActionButton: FloatingActionButton(
             onPressed: () async {
-              await authService.signOut();
+              await authService.signOut();// signOut function run from the AuthService class
               Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
                   MaterialPageRoute(
-                    builder: (context) => WelcomeScreen(),
+                    builder: (context) => WelcomeScreen(),// all screens are popped off the stack until the Welcome Screen is reached
                   ),
                   (route) => false);
             },
-            child: const Icon(Icons.logout)),
+            child: const Icon(Icons.logout)),// a logout icon is shown on the Floating Action Button
         body: Container(
           decoration: BoxDecoration(
-              color: Colors.blue[900],
+              color: Colors.blue[900], // default background colour in case the image does not load
               image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('lib/assets/profile_screen_bg.jpeg'))),
-          child: ListView(physics: BouncingScrollPhysics(), children: <Widget>[
+                  fit: BoxFit.cover, // ensures that the image takes up the entire screen
+                  image: AssetImage('lib/assets/profile_screen_bg.jpeg'))),// background image for the screen
+          child: ListView(
+            physics: BouncingScrollPhysics(),// ensures that the container bounces back from the edge
+           children: <Widget>[
             SizedBox(height: 30),
-            customWidgets.buildInfo(),
+            customWidgets.buildInfo(), // function which builds all the info widgets on the screen
             SizedBox(height: 20), 
+            // cirlce button which leads the user to the Walkthrough
             CircleButton('Walkthrough', () {
               pushNewScreenWithRouteSettings(context,
                   screen: WalkthroughScreen(),
                   settings: RouteSettings(),
-                  withNavBar: false);
+                  withNavBar: false);// the navigation bar is not shown on the Walkthrough screen
             })
           ]),
         ));
